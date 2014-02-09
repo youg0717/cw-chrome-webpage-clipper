3eb42c17a00c29b3b53521b04dd43d0b20be70d2
yuji <youg0717@gmail.com>
2014-02-09 12:20:13 +0900

    リンククリックでリンク先のタイトルとURLを送信できるようにした

--- script.js
+++ script.js
@@ -1,15 +1,51 @@
-chrome.contextMenus.create({
-  title: "表示中のタイトルとURLをChatWorkへ送信する",
-  onclick: function(info, tab) {
-  	var body = tab.title + '\n' + tab.url;
+(function(){
+    chrome.contextMenus.create({
+      title: "このリンクのタイトルとURLをChatWorkへ送信する",
+      contexts: ['link'],
+      onclick: function(info) {
+        var link_url = info.linkUrl;
 
-    var xhr = new XMLHttpRequest();
-    var api_token = localStorage.token;
-    var room_id = localStorage.room_id;
-    xhr.open("POST", 'https://api.chatwork.com/v1/rooms/' + room_id + '/messages', true);
+        getTitleFromUrl(link_url, info.selectionText, function(title) {
+             var content = title + '\n' + link_url;
+             sendToChatWork(content);
+        })
+      }
+    });
 
-    xhr.setRequestHeader('X-ChatWorkToken', api_token);
-    xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
-    xhr.send('body=' + encodeURIComponent(body)); 
-  }
-});
+    chrome.contextMenus.create({
+      title: "このページのタイトルとURLをChatWorkへ送信する",
+      onclick: function(info, tab) {
+        var content = tab.title + '\n' + tab.url;
+        sendToChatWork(content);
+      }
+    });
+    
+    var sendToChatWork = function(content) {
+        var xhr = new XMLHttpRequest();
+        var api_token = localStorage.token;
+        var room_id = localStorage.room_id;
+        xhr.open("POST", 'https://api.chatwork.com/v1/rooms/' + room_id + '/messages', true);
+
+        xhr.setRequestHeader('X-ChatWorkToken', api_token);
+        xhr.setRequestHeader("Content-Type" , "application/x-www-form-urlencoded");
+        xhr.send('body=' + encodeURIComponent(content));  
+    };
+
+    var getTitleFromUrl = function(url, default_title, callback) {
+        var title = default_title || 'unknown';
+
+        try {
+            $.get(url, function(data){
+                var matches = data.match(/<title[^>]*>([^<]+)</i);
+                if (matches) {
+                    title = matches[1];
+                }
+                callback(title);
+            });
+        } catch(e) {
+            callback(title);
+        }
+    }
+}());
+
+
